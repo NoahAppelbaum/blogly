3a0767ef734122b52bbb33954805702c7055a6da
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-30 15:35:47 -0700

    created html templates and started on routes

--- app.py
+++ app.py
@@ -2,21 +2,37 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, flash
 from models import db, connect_db, User
+# from flask_debugtoolbar import DebugToolbarExtension
+# TODO: ask why flask debug tool bar won't let us run flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SECRET_KEY'] = 'aouiergjhalwekjghio23u'
+
+# debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
-
 @app.get("/")
+def redirect_to_list():
+    """ Redirects users to list of users """
+
+    return redirect("/users")
+
+@app.get("/users")
 def list_users():
     """List users and show add form."""
 
     users = User.query.all()
     return render_template("list.html", users=users)
+
+@app.get("/users/new")
+def add_user_form():
+    """Shows add form for users""""
+
+    return render_template("userform.html")
--- models.py
+++ models.py
@@ -34,5 +34,5 @@ class User(db.Model):
     image_url = db.Column(
         db.String,
         default="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg",
-        nullable=False
+        nullable=True
     )
