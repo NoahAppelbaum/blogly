ce08eb010567e0ddfeeda42bec4741937eb347e9
Noah Appelbaum <noahappelbaum@gmail.com>
2023-08-30 14:24:53 -0500

    made User model and began app and template skeletons

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import os
 
 from flask import Flask
-from models import connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +12,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+@app.get("/")
+def list_users():
+    """List users and show add form."""
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
